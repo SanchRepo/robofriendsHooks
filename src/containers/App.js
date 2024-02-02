3cfd83d0347bc8d4279792ec7f9120e4aa207390
ProteanAbd <sanchethoque@gmail.com>
2024-02-01 21:26:58 -0500

    Converted projected into hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBar from "../components/SearchBar";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary"
 import './App.css';
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield : ''
-		}
+function App() {
 
-	}
+	const [robots, setRobots] = useState([]);
+	const [searchfield,setSearchField] = useState('')
 
-	componentDidMount() {
+// useEffect is similiar to componentDidMount when is has the dependencies
+// as an empty []. if it was let's say [robot], then it would run everytime App
+// is run. useEffect runs immediately after App is mounted
+	useEffect(()=>{
+		getRobots();
+	},[]);
+
+	const getRobots = () => {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
-			.then(users => this.setState({robots:users}))
+			.then(users => setRobots(users))
 
 	}
 
-	onChangeSearch = (event) => {
-		this.setState({searchfield: event.target.value})
-
-		}
-
+		
 	
 
-	render() {
-		const {robots, searchfield} = this.state;
-		const filteredBots = robots.filter(robot => {
-			return robot.name.toLowerCase()
-			.includes(searchfield.toLowerCase())
 
 
 
-		})
+	const onChangeSearch = (event) => {
+		setSearchField(event.target.value)
+
+	}
+
+	const filteredBots = robots.filter(robot => {
+		return robot.name.toLowerCase()
+		.includes(searchfield.toLowerCase())
+
+	})		
+
+		//const {robots, searchfield} = this.state;
 
 		if (!robots.length) {
 			return <h1 className="tc">Loading...</h1>;
 		} else {
 
 			return (
-				<React.Fragment>
+				<>
 					<div className="tc">
 						<div>
 							<h1 className="f1">RoboFriends</h1>
-							<SearchBar searchChange={this.onChangeSearch}/>
+							<SearchBar searchChange={onChangeSearch}/>
 						</div>
 						<Scroll>
 							<ErrorBoundary>
@@ -56,14 +60,14 @@ class App extends React.Component {
 							</ErrorBoundary>
 						</Scroll>
 					</div>
-				</React.Fragment>
+				</>
 
 			)
 		}
 
 
 
-	}
+	
 
 }
 
